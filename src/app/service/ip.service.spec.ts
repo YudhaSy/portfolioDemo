a2cd1df67670337dc24f22d5e40c6bd6c286095e
Yudha Syarifudin <yudha@yudhasy.com>
2020-07-02 20:45:33 -0600

    Update ip.service.spec.ts

--- src/app/service/ip.service.spec.ts
+++ src/app/service/ip.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IpService } from './ip.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('IpService', () => {
   let service: IpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(IpService);
   });
 
